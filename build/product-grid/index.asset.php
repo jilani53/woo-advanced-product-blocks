6f00a675c67e5450341591ce74095ee8c47fc607
Jilani Ahmed <jilani.cse@gmail.com>
2026-04-01 13:10:04 +0600

    Build code

--- build/product-grid/index.asset.php
+++ build/product-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '9747c150d27c2be73401');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => '16ea6c84d7d4bc7ca071');
