d1f3ccec98b220b1dec99826159ba79775b538e1
Jilani Ahmed <jilani.cse@gmail.com>
2026-04-03 14:49:11 +0600

    Improve style

--- build/product-grid/index.asset.php
+++ build/product-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '14eb2240e9671ed38160');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '99aef686aa2fbd19d3d1');
